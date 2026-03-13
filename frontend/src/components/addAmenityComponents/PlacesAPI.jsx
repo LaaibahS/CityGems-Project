06db920bea4c42895c25dfd0e9f63a201aa0f31f
Laaibah Shaad <Laaibah.shaad@city.ac.uk>
2026-03-13 15:55:51 +0000

    removed loadScript due to rendering issues when autcompleing searches. autocomplete search will add markers and pan when place is selected

--- frontend/src/components/addAmenityComponents/PlacesAPI.jsx
+++ frontend/src/components/addAmenityComponents/PlacesAPI.jsx
@@ -1,5 +1,5 @@
-import { GoogleMap, LoadScript, Marker, Autocomplete} from '@react-google-maps/api'
-import { useState,useRef } from 'react'
+import { GoogleMap, useJsApiLoader, Marker, Autocomplete} from '@react-google-maps/api'
+import { useState, useRef } from 'react'
 
 const mapContainerStyle ={
     height: "900px",
@@ -7,31 +7,61 @@ const mapContainerStyle ={
 }
 
 //geometry values for city uni
-const centre = {
+const uniCentre = {
     lat: 51.5280,
     lng: -0.1024
 }
 
 function PlacesAPI (){
 
-    const [markerPos, setMarkerPos] = useState(null);
+    const [markerPos, setmarkerPos] = useState(null)
+    const [centre, setCentre] = useState(uniCentre)
     const autocompleteRef = useRef(null)
 
     const apiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY2
 
+    const { isLoaded } = useJsApiLoader({
+        googleMapsApiKey: apiKey,
+        libraries: ["places"]
+    })
+
+    if (!isLoaded) {
+        return (
+            <div>Loading Map...</div>
+        )
+    }
+
+    const handlePlaceChanged = () => {
+
+        const place = autocompleteRef.current.getPlace();
+
+        if (!place.geometry) return;
+
+        const location = {
+            lat: place.geometry.location.lat(),
+            lng: place.geometry.location.lng()
+        };
+
+        setmarkerPos(location)
+        setCentre(location)
+  };
+
     return(
          <div>
-            <LoadScript
-                googleMapsApiKey= {apiKey}
-                library = {["places"]}>
-                <GoogleMap
-                    mapContainerStyle={mapContainerStyle}
-                    center = {centre}
-                    zoom = {15}>
-                    <Marker
-                        position={centre}/>
-                </GoogleMap>
-            </LoadScript>
+            <Autocomplete 
+            onLoad={(autocomplete) => (autocompleteRef.current = autocomplete)} 
+            onPlaceChanged={handlePlaceChanged }
+            options= {{componentRestrictions: {country: "gb"}, types: "establishment"}}>
+
+                <input id= "mapSearchBox" placeholder='search for a place' type= "text"></input>
+
+            </Autocomplete>
+            <GoogleMap
+            mapContainerStyle={mapContainerStyle}
+            center = {centre}
+            zoom = {15}>
+                {markerPos && <Marker position={markerPos}/> }
+            </GoogleMap>
         </div>
     )
 }
